5e0147546215898bba8a5e26c70e8eab3241cfb2
“Edward <eminaya49@gmail.com>
2023-07-05 15:04:24 -0500

    created user model and seeded users into database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,40 @@
+# User Sample Dataset
+user = User.create(
+  name: "Henry",
+  email: "henry@test.com",
+  password: "123",
+  password_confirmation: "123",
+  title: "Operator",
+  admin: false,
+)
+
+user = User.create(
+  name: "Sam",
+  email: "sam@test.com",
+  password: "123",
+  password_confirmation: "123",
+  title: "Operator",
+  admin: false,
+)
+
+user = User.create(
+  name: "Paul",
+  email: "paul@test.com",
+  password: "123",
+  password_confirmation: "123",
+  title: "QA Manager",
+  admin: true,
+)
+
+user = User.create(
+  name: "Phil",
+  email: "phil@test.com",
+  password: "123",
+  password_confirmation: "123",
+  title: "QA Engineer",
+  admin: true,
+)
+
 # Department Sample Dataset
 department = Department.create(
   user_id: 1,
