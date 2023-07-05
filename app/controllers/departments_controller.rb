85301b792c4e024aa8e79a73ae3037a7b1c44135
“Edward <eminaya49@gmail.com>
2023-07-05 14:18:28 -0500

    made create action and test with http request

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -8,4 +8,14 @@ class DepartmentsController < ApplicationController
     @department = Department.find_by(id: params[:id])
     render :show
   end
+
+  def create
+    @department = Department.new(
+      user_id: params[:user_id], # Later change to current_user.id
+      depart_name: params[:depart_name],
+      depart_manager: params[:depart_manager],
+    )
+    @department.save
+    render :show
+  end
 end
